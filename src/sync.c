733e79239ec17e7320f3717e3b8bbaeeb9627156
edvieria <evvasconcelos@hotmail.com>
2025-03-05 18:31:26 +0000

    added some description of the usage of the functions

--- src/sync.c
+++ src/sync.c
@@ -6,13 +6,15 @@
 /*   By: edvieira <edvieira@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/05 18:03:54 by edvieira          #+#    #+#             */
-/*   Updated: 2025/03/05 18:25:23 by edvieira         ###   ########.fr       */
+/*   Updated: 2025/03/05 18:30:59 by edvieira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/philosopher.h"
 
-// goes inside thread
+/// @brief Function for inside each thread so it will
+/// @brief be forced to wait in for the other ones bf start
+/// @param philo
 void	force_sync(t_philo *philo)
 {
 	philo->on = TRUE;
@@ -23,7 +25,9 @@ void	force_sync(t_philo *philo)
 		usleep(500);
 }
 
-// table thread
+/// @brief Routine dor table, that will basecally
+/// @brief wait for all the threads to be setted
+/// @param prog_img
 void	*waiting_sync(void *prog_img)
 {
 	t_all	*prog;
